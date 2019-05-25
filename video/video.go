ad15b1be66065e5e74229c1ea9bd4401ca1a18c7
Andre Macedo <andremacedo.pv@gmail.com>
2019-05-25 14:53:16 -0300

    Salvar os videos processados

--- video/video.go
+++ video/video.go
@@ -5,18 +5,32 @@ import (
 )
 
 func main() {
+	fps := 15
+	// Open video capture
 	webcam, _ := gocv.OpenVideoCapture("keyboard.mp4")
+	// Initialize output
+	vid_width := int(webcam.Get(3))
+	vid_height := int(webcam.Get(4))
+	output, _ := gocv.VideoWriterFile("ToonVideo.avi","MJPG",float64(fps),vid_width,vid_height,true)
+
 	window := gocv.NewWindow("Hello")
 	window2 := gocv.NewWindow("Hello2")
 	img := gocv.NewMat()
-	fps := 5
+	ret := true
 
 	for {
-		webcam.Read(&img)
+		ret = webcam.Read(&img)
+		if !ret{
+			break;
+		}
 		img2 := toonify(img)
 		window.IMShow(img)
 		window2.IMShow(img2)
+		output.Write(img2)
 		window.WaitKey(1000 / fps)
 	}
+
+	webcam.Close()
+	output.Close()
 
 }
