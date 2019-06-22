b0fc054172d1b470d5f5b864399979083175a03b
Kálley Wilkerson <yellakkalley@gmail.com>
2019-06-21 22:37:03 -0300

    Assertivas para a toonify.go

--- toonify.go
+++ toonify.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"Toonify/kmeans"
-	"log"
-	"time"
-
+	"fmt"
 	"gocv.io/x/gocv"
+	"log"
+	"os"
+	"time"
 )
 
 //Reconstroi a imagem a partir da clusterização do kmeans
@@ -56,16 +57,29 @@ func formatData(img gocv.Mat) []kmeans.ClusteredObservation {
 }
 
 func main() {
-
 	start := time.Now()
 
-	img := gocv.IMRead("gopher.png", gocv.IMReadUnchanged)
+	if len(os.Args) < 2 {
+		fmt.Println("How to run:\n\t./toonify [image file]")
+		return
+	}
+
+	img := gocv.IMRead(os.Args[1], gocv.IMReadUnchanged)
+	if img.Empty() {
+		fmt.Println("Couldn't open image")
+		return
+	}
+
+	fmt.Println("Toonifying...")
+	gocv.IMWrite("result/1original.jpg", img)
 	defer img.Close()
 
 	// borrando imagem
 	imgBlured := gocv.NewMat()
 	defer imgBlured.Close()
 	gocv.MedianBlur(img, &imgBlured, 7)
+	gocv.IMWrite("result/2blured.jpg", imgBlured)
+	fmt.Println("Blured")
 
 	// fazendo as bordas
 	imgEdges := gocv.NewMat()
@@ -73,20 +87,32 @@ func main() {
 	gocv.Canny(imgBlured, &imgEdges, 62.5, 125)
 	gocv.BitwiseNot(imgEdges, &imgEdges)
 	gocv.CvtColor(imgEdges, &imgEdges, gocv.ColorGrayToBGR)
+	gocv.IMWrite("result/5edges.jpg", imgEdges)
+	fmt.Println("Edges taken")
 
 	// aplicando filtro bilateral
 	imgFiltered := gocv.NewMat()
 	defer imgFiltered.Close()
 	gocv.BilateralFilter(imgBlured, &imgFiltered, 7, 35, 35)
+	gocv.IMWrite("result/3bfilter.jpg", imgFiltered)
+	fmt.Println("Filtered")
 
-	// cartunizando
+	// quantização da imagem
 	data := formatData(imgFiltered)
 	clusteredData, centroids, _ := kmeans.Kmeans(data, 24, kmeans.EuclideanDistance, 10)
 	imgQuantized, _ := imgRework(clusteredData, centroids, img.Rows(), img.Cols())
 	defer imgQuantized.Close()
-	imgToonify := gocv.NewMat()
-	gocv.BitwiseAnd(imgEdges, imgQuantized, &imgToonify)
+	gocv.IMWrite("result/4quantized.jpg", imgQuantized)
+	fmt.Println("Quantized")
 
+	// juntando as bordas com a quantizada
+	imgToonify := gocv.NewMat()
+	defer imgToonify.Close()
+	gocv.BitwiseAnd(imgEdges, imgQuantized, &imgToonify)
+	gocv.IMWrite("result/6final.jpg", imgToonify)
+	fmt.Println("Toonified.")
+
+	// mostrando o tempo total gasto
 	elapsed := time.Since(start)
 	log.Printf("total time: %s", elapsed)
 
